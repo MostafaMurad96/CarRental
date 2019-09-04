b835068210edf8bef267fa8a5b7ce89a794fdac4
unknown <mostafamurad29@gmail.com>
2019-09-04 13:30:23 +0200

    first commit

---
+++ JavaScript/main.js
@@ -0,0 +1,8 @@
+function hidesignin() {
+    var x=document.getElementById("sign-form");
+    x.style.display = "none";
+}
+function showsignin() {
+    var x=document.getElementById("sign-form");
+    x.style.display="block";
+}
